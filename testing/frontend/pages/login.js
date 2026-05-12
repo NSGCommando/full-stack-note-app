3a8fcf301dde12219290c1b79bcfd66265a04601
NSGCommando <41550763+NSGCommando@users.noreply.github.com>
2026-05-12 23:17:58 +0530

    Playwright test update

--- testing/frontend/pages/login.js
+++ testing/frontend/pages/login.js
@@ -1,9 +1,9 @@
 export class LoginPage{
     constructor(page){
         this.page = page;
-        this.loadingAnim = page.locator(".loading-screen");
-        this.loginContainer = page.locator(".login-container");
-        this.signupContainer = page.locator(".signup-container");
+        this.loadingAnim = page.locator("#loading-screen");
+        this.loginContainer = page.locator("#login-container");
+        this.signupContainer = page.locator("#signup-button-container");
     }
     /**
      * Function to wait till load animation is not visible after 2 seconds and login container is.
